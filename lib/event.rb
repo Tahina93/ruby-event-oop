8a3539dfb65c039aa045930e36affbe026969d16
Tahina93 <anne.tahina.metz@gmail.com>
2019-04-15 12:20:52 +0200

    première partie de l'exo

--- lib/event.rb
+++ lib/event.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 class Event
 
 end
--- lib/user.rb
+++ lib/user.rb
@@ -1,3 +1,40 @@
+require "pry"
+
+
 class User
+
+  attr_accessor :email, :age
+  @@all_user = []
+  
+  def initialize(email, age)
+  	@email = email
+  	@age = age
+  	@@all_user << self
+
+  	puts "On envoie un email de Bienvenue !!"
+  end
+
+  def update_email(email_to_update)
+    @email = email_to_update
+  end
+
+  def read_email
+    return @email
+  end
+
+  def update_age(age_to_update)
+    @age = age_to_update
+  end
+
+  def read_age
+  	return @age
+end
+
+  def self.all
+  	return @@all_user
+  end
+
+binding.pry
+puts "end of file"
 
 end
